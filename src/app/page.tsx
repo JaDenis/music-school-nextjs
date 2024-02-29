bf71f49d62b263de51001605c8bbbeab1f022b14
Mahapara <mahanizamani12345678@gmail.com>
2024-02-29 23:09:52 +0500

    instructor component added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import WhyChooseUs from "@/components/WhyChooseUs";
 import TestimonialCards from "@/components/TestimonialCards";
 import Image from "next/image";
 import UpcomingWebinars from "@/components/UpcomingWebinars";
+import Instructors from "@/components/Instructors";
 
 export default function Home() {
   return (
@@ -13,6 +14,7 @@ export default function Home() {
       <WhyChooseUs />
       <TestimonialCards />
       <UpcomingWebinars />
+      <Instructors />
     </main>
   );
 }
